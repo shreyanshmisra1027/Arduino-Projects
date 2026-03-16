7be7fa287dc7805c208091bb747f3aec8ba995c1
Shreyansh Misra <misrashreyansh6@gmail.com>
2026-03-16 19:42:44 +0530

    this one for sure

---
+++ sketches/ledfade.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(9, OUTPUT);
+}
+void loop() {
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(9, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(9, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+}
